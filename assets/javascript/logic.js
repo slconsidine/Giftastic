8ea3a7f4f76294f599a812d83c742625bd27697c
Sam <samlconsidine@gmail.com>
2020-02-03 14:49:37 -0500

    Add your own button feature enabled.

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -5,16 +5,19 @@ $(document).ready(function() {
 
     // for loop that creates a button for each item in the buttons array
     for (i=0; i<buttons.length; i++) {
-        $("#btn-holder").append("<button id='" + buttons[i] + "'>" + buttons[i] + "</button>");
+        $("#btn-holder").append("<button class='premade' id='" + buttons[i] + "'>" + buttons[i] + "</button>");
     }
 
+
     // process that occurs when a button is clicked
-    $("button").on("click", function(){
+    $(".premade").on("click", function(){
+        // clears div so that new ones can be added and aren't just adding on and on 
+        $("#gifs-appear-here").empty();
         // creates a variable with what button is calling by using the button's id
-        var animal = $(this).attr("id");
-        console.log($(this).attr("id"));
+        var office = $(this).attr("id");
+        console.log(office);
         // makes a query using the button name
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=mQCa78tyW0RZHnb9NccCWNSey9mFbl6p&limit=10";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + office + "&api_key=mQCa78tyW0RZHnb9NccCWNSey9mFbl6p&limit=10";
         // makes an AJAX request
         $.ajax({
             url: queryURL,
@@ -26,17 +29,17 @@ $(document).ready(function() {
             var results = response.data;
             // loops through each result item
             for (var i=0; i<results.length; i++) {
-                var animalDiv = $("<div>");
+                var officeDiv = $("<div>");
                 var rating = $("<p>").text("Rating: " + results[i].rating);
-                var animalImage = $("<img>");
-                animalImage.attr("src", results[i].images.fixed_height_still.url);
-                animalImage.addClass("gif");
-                animalImage.attr("data-still", results[i].images.fixed_height_still.url);
-                animalImage.attr("data-animate", results[i].images.fixed_height.url);
-                animalImage.attr("data-state", "still");
-                animalDiv.append(rating);
-                animalDiv.append(animalImage);
-                $("#gifs-appear-here").prepend(animalDiv);
+                var officeImage = $("<img>");
+                officeImage.attr("src", results[i].images.fixed_height_still.url);
+                officeImage.addClass("gif");
+                officeImage.attr("data-still", results[i].images.fixed_height_still.url);
+                officeImage.attr("data-animate", results[i].images.fixed_height.url);
+                officeImage.attr("data-state", "still");
+                officeDiv.append(rating);
+                officeDiv.append(officeImage);
+                $("#gifs-appear-here").prepend(officeDiv);
             };
             // process that occurs when a gif is clicked
             $(".gif").on("click", function() {
@@ -54,4 +57,13 @@ $(document).ready(function() {
             });
         });
     });
+
+    // add a new user search button
+    $("#submit").on("click", function() {
+        event.preventDefault();
+        var searchTerm = $("#search-query").val()
+        console.log(searchTerm);
+        $("#btn-holder").append("<button class='premade' id='" + searchTerm+ "'>" + searchTerm + "</button>");
+    })    
+
 });
